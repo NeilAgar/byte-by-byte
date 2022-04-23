fb35c83c44ba1b23fea774d8f7be0675520eb863
NeilAgar <aditneil@outlook.com>
2022-04-23 18:44:44 -0500

    Switched mailing domain

--- default_config.py
+++ default_config.py
@@ -3,11 +3,11 @@ import os
 SECRET_KEY = os.environ["APP_SECRET_KEY"]
 
 MAIL_DEBUG = True
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
-MAIL_USERNAME = "bytebybyte.npo@gmail.com"
+MAIL_SERVER = "smtp.sendgrid.net"
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = "apikey"
 MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
 MAIL_DEFAULT_SENDER = "bytebybyte.npo@gmail.com"
 MAIL_MAX_EMAILS = None
