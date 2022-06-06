a5f7564eafdaeadb75f3b17717980939d10616f4
NeilAgar <aditneil@outlook.com>
2022-06-06 15:42:17 -0500

    Fix spam

--- resources/home.py
+++ resources/home.py
@@ -9,23 +9,25 @@ class HomePage(Resource):
     @classmethod
     def get(cls):
         headers = {"Content-Type": "text/html"}
-        return make_response(render_template("home_page.html"), 200, headers,)
+        return make_response(render_template("home_page.html"), 200, headers, )
 
     @classmethod
     def post(cls):
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
+        if name not in ["HenryPhiBe"]:
+            msg = Message("Byte By Byte Website Question",
+                          recipients=["neiliscool67@gmail.com", "aditagarwal76@gmail.com", "bytebybyte.npo@gmail.com",
+                                      "aditneil@outlook.com"])
 
-        msg = Message("Byte By Byte Website Question", recipients=["neiliscool67@gmail.com", "aditagarwal76@gmail.com", "bytebybyte.npo@gmail.com", "aditneil@outlook.com"])
+            msg.body = f"""
+    Name: {name}
+    Email: {email}
 
-        msg.body = f"""
-Name: {name}
-Email: {email}
+    {message}
+    """
 
-{message}
-"""
+            mail.send(msg)
 
-        mail.send(msg)
-
-        return redirect(url_for("homepage"))
+            return redirect(url_for("homepage"))
