ff8fa3eb13f49495689cf631502b4661df61c7d1
NeilAgar <aditneil@outlook.com>
2022-06-13 11:18:44 -0500

    Log requests

--- resources/home.py
+++ resources/home.py
@@ -8,6 +8,7 @@ from extensions import mail
 class HomePage(Resource):
     @classmethod
     def get(cls):
+        print(request.headers)
         headers = {"Content-Type": "text/html"}
         return make_response(render_template("home_page.html"), 200, headers, )
 
